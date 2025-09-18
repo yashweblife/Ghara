6df50b18b4bd77342c87f81c6480f6be152a6705
yashweblife <yashweblife20@gmail.com>
2025-09-18 17:21:09 -0500

    add routes idea

---
+++ server/src/routes/user-route.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+
+const router = Router();
+
+/**
+ * Routes for user
+ * get    /user
+ * get    /user/:id
+ * get    /user/:id/points
+ * post   /user
+ * put    /user/:id
+ * delete /user/:id
+ */
