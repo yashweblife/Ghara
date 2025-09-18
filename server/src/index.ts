ea3f13ba5eef39d8467f287a16b4ab1fe95038b4
yashweblife <yashweblife20@gmail.com>
2025-09-18 17:34:35 -0500

    fix

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,5 +2,5 @@ import app from './app';
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log('Server started on port 3000');
+    console.log(`Server started on port ${PORT}`);
 });
