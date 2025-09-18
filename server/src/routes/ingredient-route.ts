b5674f602af2840321dec781a0d72aefa0c46d0e
yashweblife <yashweblife20@gmail.com>
2025-09-18 17:22:00 -0500

    add routes idea

---
+++ server/src/routes/ingredient-route.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+
+const router = Router();
+
+/**
+ * Routes for ingredient
+ * get    /ingredient
+ * get    /ingredient/:id
+ * post   /ingredient
+ * put    /ingredient/:id
+ * delete /ingredient/:id
+ */
