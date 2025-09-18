316c8514a3e4ffe4d793a47928e4539801a142e1
yashweblife <yashweblife20@gmail.com>
2025-09-18 17:23:54 -0500

    add routes idea

---
+++ server/src/routes/product-route.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+
+const router = Router();
+/**
+ * Routes for product
+ * get    /product
+ * get    /product/:id
+ * post   /product
+ * put    /product/:id
+ * delete /product/:id
+ */
