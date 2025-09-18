16f4d5d65d22480d33d514dd61281941988257f1
yashweblife <yashweblife20@gmail.com>
2025-09-18 17:23:24 -0500

    add routes idea

---
+++ server/src/routes/point-route.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+
+const router = Router();
+/**
+ * Routes for point
+ * get    /point
+ * get    /point/:id
+ * post   /point
+ * put    /point/:id
+ * delete /point/:id
+ */
+
+export default router;
