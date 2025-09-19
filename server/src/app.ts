1876c6000b892bed27d935c8cb51777134e2a961
yashweblife <yashweblife20@gmail.com>
2025-09-18 19:05:35 -0500

    add 404 handler

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,16 @@
 import cors from 'cors';
-import express from 'express';
-
+import express, { type Request, type Response } from 'express';
+import FoodRoute from './routes/food-route';
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/api', FoodRoute);
+app.get('/', (req: Request, res: Response) => {
+    res.send('hello world');
+})
 
-
+app.use((req: Request, res: Response) => {
+    res.status(404).send('<h1 style="font-family:Arial , Helvetica, sans-serif">Bad Request</h1>');
+})
 export default app;
