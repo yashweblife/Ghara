3a58d09f03d64fb86a1c6e5bb0100405c9c291ca
yashweblife <yashweblife20@gmail.com>
2025-09-19 14:07:04 -0500

    use controllers

--- server/src/routes/point-route.ts
+++ server/src/routes/point-route.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { getAllPoints, getPointById, createPoint, updatePointById, deletePointById } from "../controllers/point-controller";
 
 const router = Router();
 /**
@@ -9,5 +10,10 @@ const router = Router();
  * put    /point/:id
  * delete /point/:id
  */
-
+router
+    .get('/', getAllPoints)
+    .get('/:id', getPointById)
+    .post('/', createPoint)
+    .put('/:id', updatePointById)
+    .delete('/:id', deletePointById);
 export default router;
