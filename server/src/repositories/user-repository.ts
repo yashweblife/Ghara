82c0bac3d8483c7ba6ecfdc74e5922faf99fa6dc
yashweblife <yashweblife20@gmail.com>
2025-09-19 14:29:49 -0500

    fix type of id

--- server/src/repositories/user-repository.ts
+++ server/src/repositories/user-repository.ts
@@ -4,10 +4,10 @@ const prisma = new PrismaClient();
 
 const repository = {
     findAll: async () => await prisma.user.findMany(),
-    findById: async (id: number) => await prisma.user.findUnique({ where: { id } }),
-    findUserPoints: async (id: number) => await prisma.point.findMany({ where: { userId: id } }),
+    findById: async (id: string) => await prisma.user.findUnique({ where: { id } }),
+    findUserPoints: async (id: string) => await prisma.point.findMany({ where: { userId: id } }),
     create: async (name: string, password: string) => await prisma.user.create({ data: { name, password } }),
-    update: async (id: number, name: string, password: string) => await prisma.user.update({ where: { id }, data: { name, password } }),
-    delete: async (id: number) => await prisma.user.delete({ where: { id } })
+    update: async (id: string, name: string, password: string) => await prisma.user.update({ where: { id }, data: { name, password } }),
+    delete: async (id: string) => await prisma.user.delete({ where: { id } })
 }
 export default repository;
