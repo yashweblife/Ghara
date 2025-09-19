e1397bde389a98383e97d14538d413d9a90c282a
yashweblife <yashweblife20@gmail.com>
2025-09-19 13:59:55 -0500

    controllers

---
+++ server/src/controllers/product-controller.ts
@@ -0,0 +1,7 @@
+import type { Request, Response } from "express";
+import { toBeImplemented } from "../utils";
+export function getAllProducts(req: Request, res: Response) { toBeImplemented(req, res) }
+export function getProductsById(req: Request, res: Response) { toBeImplemented(req, res) }
+export function createProduct(req: Request, res: Response) { toBeImplemented(req, res) }
+export function updateProductById(req: Request, res: Response) { toBeImplemented(req, res) }
+export function deleteProductById(req: Request, res: Response) { toBeImplemented(req, res) }
