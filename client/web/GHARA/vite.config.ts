98d6c033b94eea7831d6d5b8b6642d9234a1cd65
yashweblife <yashweblife20@gmail.com>
2025-09-19 11:52:34 -0500

    add proxy

--- client/web/GHARA/vite.config.ts
+++ client/web/GHARA/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5170',
+        changeOrigin: true,
+        secure: false
+      },
+
+    }
+  }
 })
