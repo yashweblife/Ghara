26fc8421dd70b84c7155e7f7091a87ee2e3a458a
yashweblife <yashweblife20@gmail.com>
2025-09-19 14:24:00 -0500

    add function

--- server/src/controllers/user-controller.ts
+++ server/src/controllers/user-controller.ts
@@ -1,8 +1,33 @@
 import type { Request, Response } from "express";
-import { toBeImplemented } from "../utils";
-export function getAllUsers(req: Request, res: Response) { toBeImplemented(req, res) }
-export function getUserById(req: Request, res: Response) { toBeImplemented(req, res) }
-export function getUserPoints(req: Request, res: Response) { toBeImplemented(req, res) }
-export function createUser(req: Request, res: Response) { toBeImplemented(req, res) }
-export function updateUserById(req: Request, res: Response) { toBeImplemented(req, res) }
-export function deleteUserById(req: Request, res: Response) { toBeImplemented(req, res) }
+import userRepo from "../repositories/user-repository";
+import {
+    checkIfNullish,
+    ISE,
+    toBeImplemented
+} from "../utils";
+export function getAllUsers(req: Request, res: Response) {
+    try {
+        const users = userRepo.findAll();
+        if (checkIfNullish(users)) {
+            throw { status: 500, message: 'Failed to fetch all users\nUsers was nullish ' }
+        }
+        res.status(200).json(users);
+    } catch (err: any) {
+        res.status(err.status || 500).send(err.message || ISE)
+    }
+}
+export function getUserById(req: Request, res: Response) {
+    toBeImplemented(req, res)
+}
+export function getUserPoints(req: Request, res: Response) {
+    toBeImplemented(req, res)
+}
+export function createUser(req: Request, res: Response) {
+    toBeImplemented(req, res)
+}
+export function updateUserById(req: Request, res: Response) {
+    toBeImplemented(req, res)
+}
+export function deleteUserById(req: Request, res: Response) {
+    toBeImplemented(req, res)
+}
