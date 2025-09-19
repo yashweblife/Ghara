26edbc883a4609f085f44736950928526ac93479
yashweblife <yashweblife20@gmail.com>
2025-09-19 14:06:58 -0500

    controllers

---
+++ server/src/controllers/point-controller.ts
@@ -0,0 +1,8 @@
+import type { Request, Response } from "express";
+import { toBeImplemented } from "../utils";
+export function getAllPoints(req: Request, res: Response) { toBeImplemented(req, res) }
+export function getPointById(req: Request, res: Response) { toBeImplemented(req, res) }
+export function createPoint(req: Request, res: Response) { toBeImplemented(req, res) }
+export function updatePointById(req: Request, res: Response) { toBeImplemented(req, res) }
+export function deletePointById(req: Request, res: Response) { toBeImplemented(req, res) }
+
