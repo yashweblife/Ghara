69454b805f9fc7907c1a1412ea6a2e3f9fb77c94
yashweblife <yashweblife20@gmail.com>
2025-09-19 12:39:12 -0500

    new controllers

---
+++ server/src/controllers/user-controller.ts
@@ -0,0 +1,5 @@
+export function getAllUsers(req: Request, res: Response) { }
+export function getUserById(req: Request, res: Response) { }
+export function createUser(req: Request, res: Response) { }
+export function updateUserById(req: Request, res: Response) { }
+export function deleteUserById(req: Request, res: Response) { }
