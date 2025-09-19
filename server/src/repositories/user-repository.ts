a7dea43dbf8886549fe1fbf70ef723078ce70b5a
yashweblife <yashweblife20@gmail.com>
2025-09-19 14:14:30 -0500

    add repo

---
+++ server/src/repositories/user-repository.ts
@@ -0,0 +1,12 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const UserRepository = {
+    findAll: async () => await prisma.user.findMany(),
+    findById: async (id: number) => await prisma.user.findUnique({ where: { id } }),
+    findUserPoints: async (id: number) => await prisma.point.findMany({ where: { userId: id } }),
+    create: async (name: string, password: string) => await prisma.user.create({ data: { name, password } }),
+    update: async (id: number, name: string, password: string) => await prisma.user.update({ where: { id }, data: { name, password } }),
+    delete: async (id: number) => await prisma.user.delete({ where: { id } })
+}
