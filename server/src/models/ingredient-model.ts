231cdb6ecb5362b525b388b2f70665502264e047
yashweblife <yashweblife20@gmail.com>
2025-09-19 15:58:07 -0500

    add converter

--- server/src/models/ingredient-model.ts
+++ server/src/models/ingredient-model.ts
@@ -1,5 +1,15 @@
+import type { Ingredient } from "@prisma/client";
+
 export interface IngredientDTO {
     id: number;
     name: string;
     quantity: number;
+}
+
+export function toIngredientDTO(ingredient: Ingredient) {
+    return {
+        id: ingredient.foodId,
+        name: ingredient.productId,
+        quantity: ingredient.quantity,
+    };
 }
