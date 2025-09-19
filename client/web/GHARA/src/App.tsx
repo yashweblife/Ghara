0cc69a5c3c30b35ae2c3ae1acb5dc27ea67dff86
yashweblife <yashweblife20@gmail.com>
2025-09-19 11:52:45 -0500

    test

--- client/web/GHARA/src/App.tsx
+++ client/web/GHARA/src/App.tsx
@@ -1,7 +1,21 @@
-import './App.css'
+import { useEffect } from "react"
 
 function App() {
-
+  useEffect(() => {
+    test()
+  }, [])
+  const test = async () => {
+    try {
+      const res = await fetch('/api/food')
+      if (res.status !== 200) {
+        throw new Error('Failed to fetch data')
+      }
+      const data = await res.json()
+      console.log(data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
   return (
     <>
 
