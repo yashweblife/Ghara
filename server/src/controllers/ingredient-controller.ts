979a01728633b9b41c67e1b86d48a53474fb1967
yashweblife <yashweblife20@gmail.com>
2025-09-19 14:08:54 -0500

    controllers

---
+++ server/src/controllers/ingredient-controller.ts
@@ -0,0 +1,7 @@
+import type { Request, Response } from "express";
+import { toBeImplemented } from "../utils";
+export function getAllIngredients(req: Request, res: Response) { toBeImplemented(req, res) }
+export function getIngredientById(req: Request, res: Response) { toBeImplemented(req, res) }
+export function createIngredient(req: Request, res: Response) { toBeImplemented(req, res) }
+export function updateIngredientById(req: Request, res: Response) { toBeImplemented(req, res) }
+export function deleteIngredientById(req: Request, res: Response) { toBeImplemented(req, res) }
