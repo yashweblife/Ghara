cc3bdb518ac56e30d3ea1491a4ade2e8ddb39edd
yashweblife <yashweblife20@gmail.com>
2025-09-19 12:38:11 -0500

    new controllers

---
+++ server/src/controllers/food-controller.ts
@@ -0,0 +1,7 @@
+import type { Request, Response } from "express";
+
+export function getAllFoods(req: Request, res: Response) { }
+export function getFoodById(req: Request, res: Response) { }
+export function createFood(req: Request, res: Response) { }
+export function updateFoodById(req: Request, res: Response) { }
+export function deleteFoodById(req: Request, res: Response) { }
