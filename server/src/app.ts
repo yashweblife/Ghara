f00cacfeb8e1be0fd6e84181cb9b07e05181041e
yashweblife <yashweblife20@gmail.com>
2025-09-18 19:42:16 -0500

    add url enconded

--- server/src/app.ts
+++ server/src/app.ts
@@ -5,6 +5,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use('/api/food', FoodRoute);
 app.get('/', (req: Request, res: Response) => {
     res.send('hello world');
