2b1a0cadf37ed15b684da35b82c551de6814df11
yashweblife <yashweblife20@gmail.com>
2025-09-19 12:39:54 -0500

    fix

--- server/src/controllers/user-controller.ts
+++ server/src/controllers/user-controller.ts
@@ -1,5 +1,7 @@
+import type { Request, Response } from "express";
 export function getAllUsers(req: Request, res: Response) { }
 export function getUserById(req: Request, res: Response) { }
+export function getUserPoints(req: Request, res: Response) { }
 export function createUser(req: Request, res: Response) { }
 export function updateUserById(req: Request, res: Response) { }
 export function deleteUserById(req: Request, res: Response) { }
