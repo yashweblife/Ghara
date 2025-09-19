f57a0d3325e3c9cf8fd186633cb0f314f23d409d
yashweblife <yashweblife20@gmail.com>
2025-09-19 13:49:24 -0500

    add tbi

--- server/src/controllers/food-controller.ts
+++ server/src/controllers/food-controller.ts
@@ -1,7 +1,8 @@
 import type { Request, Response } from "express";
+import { toBeImplemented } from "../utils";
 
-export function getAllFoods(req: Request, res: Response) { }
-export function getFoodById(req: Request, res: Response) { }
-export function createFood(req: Request, res: Response) { }
-export function updateFoodById(req: Request, res: Response) { }
-export function deleteFoodById(req: Request, res: Response) { }
+export function getAllFoods(req: Request, res: Response) { toBeImplemented(req, res) }
+export function getFoodById(req: Request, res: Response) { toBeImplemented(req, res) }
+export function createFood(req: Request, res: Response) { toBeImplemented(req, res) }
+export function updateFoodById(req: Request, res: Response) { toBeImplemented(req, res) }
+export function deleteFoodById(req: Request, res: Response) { toBeImplemented(req, res) }
