fa73a8376c5c3c4912ce92d250014c84813be215
yashweblife <yashweblife20@gmail.com>
2025-09-19 14:23:53 -0500

    more helper functions

--- server/src/utils/index.ts
+++ server/src/utils/index.ts
@@ -3,3 +3,7 @@ export function toBeImplemented(req: Request, res: Response) {
     console.warn('Method needs to be implemented')
     res.status(200).send('To be implemented')
 }
+export function checkIfNullish<T>(value: T | null | undefined): value is T {
+    return value == null && value == undefined;
+}
+export const ISE = 'Internal Server Error';
