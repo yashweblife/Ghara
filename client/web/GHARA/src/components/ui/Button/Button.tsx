313e0589566ec6d41bbc74cc6451ada005cb36d9
yashweblife <yashweblife20@gmail.com>
2025-09-22 16:25:59 -0500

    working button

--- client/web/GHARA/src/components/ui/Button/Button.tsx
+++ client/web/GHARA/src/components/ui/Button/Button.tsx
@@ -1,11 +1,35 @@
-import type { ReactNode } from "react"
+import type { ButtonHTMLAttributes, ReactNode } from "react"
 
-export type ButtonProps = {
-    children?: ReactNode
+export type ButtonProps = Omit<ButtonHTMLAttributes<HTMLButtonElement>, 'size'> & {
+    children?: ReactNode,
+    variant?: "default" | "primary" | "secondary",
+    size?: "small" | "medium" | "large",
+    className?: string
 }
-export default function Button({ children }: ButtonProps) {
+const defaultStyle = 'px-20'
+const variants = {
+    default: 'bg-red-200',
+    primary: 'bg-red-500',
+    secondary: 'bg-red-100',
+}
+const sizes = {
+    small: 'p-5',
+    medium: 'p-7',
+    large: 'p-10'
+}
+
+export default function Button({
+    className,
+    children,
+    variant = "default",
+    size = "medium",
+    ...props
+}: ButtonProps) {
+
+    const styles = `${defaultStyle} ${variants[variant]} ${sizes[size]} ${className ?? ''}`
+    console.log(styles)
     return (
-        <button>
+        <button className={styles} {...props}>
             {children}
         </button>
     )
