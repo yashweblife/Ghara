b1c111e09cce5313065f05d58af15e3cc431b6d8
yashweblife <yashweblife20@gmail.com>
2025-09-22 11:31:42 -0500

    clean

--- client/web/GHARA/eslint.config.js
+++ client/web/GHARA/eslint.config.js
@@ -1,9 +1,12 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
+import js from '@eslint/js';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import { globalIgnores } from 'eslint/config';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config([
   globalIgnores(['dist']),
@@ -20,4 +23,4 @@ export default tseslint.config([
       globals: globals.browser,
     },
   },
-])
+], storybook.configs["flat/recommended"]);
