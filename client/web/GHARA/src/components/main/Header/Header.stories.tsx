ef0ba926cc1a83c81d5119260a87b3c7888fa44a
yashweblife <yashweblife20@gmail.com>
2025-09-24 15:16:06 -0500

    header story

---
+++ client/web/GHARA/src/components/main/Header/Header.stories.tsx
@@ -0,0 +1,15 @@
+import type { Meta, StoryObj } from '@storybook/react-vite';
+
+import Header from './Header';
+
+const meta = {
+  component: Header,
+} satisfies Meta<typeof Header>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {}
+};
