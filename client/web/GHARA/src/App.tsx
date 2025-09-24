3fc0f38cb2d8d32f848281fc5797dddb3df140dc
yashweblife <yashweblife20@gmail.com>
2025-09-24 15:11:32 -0500

    add header style

--- client/web/GHARA/src/App.tsx
+++ client/web/GHARA/src/App.tsx
@@ -1,6 +1,7 @@
-import { useEffect } from "react"
-import Button from "./components/ui/Button/Button"
-
+import { useEffect } from "react";
+import { MdOutlineAccountCircle } from 'react-icons/md';
+import Logo from '../public/logo.svg';
+import Button from "./components/ui/Button/Button";
 function App() {
   useEffect(() => {
     test()
@@ -20,19 +21,23 @@ function App() {
   return (
     <>
       <header
-        className="w-full flex justify-between align-center px-10 py-5"
+        className="flex-col w-full flex justify-between px-10 py-5 md:flex-row"
       >
-        <h1
-          className="text-4xl font-bold"
-        >
-          GHARA
-        </h1>
+        <div className='flex gap-3 items-center justify-center md:w-full'>
+          <img src={Logo} alt="logo" className="w-[50px]" />
+          <h1
+            className="text-4xl font-bold text-gray-800"
+          >
+            GHARA
+          </h1>
+
+        </div>
         <nav
-          className="flex gap-5"
+          className="w-full flex gap-1 mt-5 justify-evenly"
         >
-          <Button variant="primary" size='large'>hello</Button>
-          <Button variant="primary" size='large'>hello</Button>
-          <Button variant="primary" size='large'>hello</Button>
+          <Button variant="ghost" size='sm' text='Install' />
+          <Button variant="ghost" size='sm' text='About' />
+          <Button variant="solid" size='sm' text='Login' icon={<MdOutlineAccountCircle />} />
         </nav>
       </header>
     </>
