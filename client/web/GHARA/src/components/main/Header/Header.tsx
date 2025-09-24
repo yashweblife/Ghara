a9c7d0b94699049e16c6a880ad48ddc6edde88fd
yashweblife <yashweblife20@gmail.com>
2025-09-24 17:31:47 -0500

    fix styles

--- client/web/GHARA/src/components/main/Header/Header.tsx
+++ client/web/GHARA/src/components/main/Header/Header.tsx
@@ -1,10 +1,16 @@
-import { MdOutlineAccountCircle } from 'react-icons/md';
+import { useState } from 'react';
+import { AiOutlineSun } from 'react-icons/ai';
+import { MdOutlineAccountCircle, MdOutlineDarkMode } from 'react-icons/md';
 import Logo from '../../../../public/logo.svg';
 import Button from "../../ui/Button/Button";
 export default function Header() {
+    const [themeMode, setThemeMode] = useState(false);
+    const handleThemeChange = () => {
+        setThemeMode(prev => !prev);
+    }
     return (
         <header
-            className="flex-col w-full flex justify-between px-10 py-5 md:flex-row"
+            className="flex-col w-full flex md:flex-row items-center align-middle md:justify-between md:px-5 md:py-5"
         >
             <div className='flex gap-3 items-center justify-center md:w-full'>
                 <img src={Logo} alt="logo" className="w-[50px]" />
@@ -16,10 +22,17 @@ export default function Header() {
 
             </div>
             <nav
-                className="w-full flex gap-1 mt-5 justify-evenly"
+                className="w-full flex gap-1 justify-evenly mt-3 md:mt-0"
             >
                 <Button variant="ghost" size='sm' text='Install' />
                 <Button variant="ghost" size='sm' text='About' />
+                <Button
+                    variant={themeMode ? "outlined" : 'solid'}
+                    rounded='full'
+                    size='sm'
+                    icon={themeMode ? <MdOutlineDarkMode /> : <AiOutlineSun />}
+                    onClick={handleThemeChange}
+                />
                 <Button variant="solid" size='sm' text='Login' icon={<MdOutlineAccountCircle />} />
             </nav>
         </header>
