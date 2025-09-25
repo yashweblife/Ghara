604efe6d1276c8e27779405a2c96143c0d0125a4
yashweblife <yashweblife20@gmail.com>
2025-09-25 15:30:50 -0500

    add text

--- client/web/GHARA/src/components/main/Hero/Hero.tsx
+++ client/web/GHARA/src/components/main/Hero/Hero.tsx
@@ -1,18 +1,16 @@
 import { AiFillAndroid } from "react-icons/ai";
 import Button from "../../ui/Button/Button";
 import Bold from "../../ui/Typography/Bold/Bold";
-
+import Text from "../../ui/Typography/Text/Text";
 export default function Hero() {
     return (
-        <div className='dark:bg-gray-900 bg-gray-100 h-112 p-4 sm:p-8 flex justify-center items-center flex-col gap-3'>
-            <p className='dark:text-white text-2xl md:text-4xl font-bold'>
+        <div className='dark:bg-gray-900 bg-gray-100 h-112 p-4 sm:p-8 flex justify-center items-center flex-col gap-3 bg-[url("../../../../public/bg.svg")] bg-no-repeat bg-center'>
+            <Text center className="text-2xl md:text-4xl" bold>
                 Take control of your <Bold text='Ghara' />
-            </p>
-            <p className="dark:text-white md:w-[45ch] text-center text-gray-500 md:text-2xl">
-                Manage your <Bold text='groceries' />,
-                Award yourself for <Bold text='effort' />
-                and track your < Bold text='spending' />
-            </p>
+            </Text>
+            <Text center className='md:w-[45ch] text-gray-500 md:text-2xl'>
+                Manage your <Bold text='groceries' />, Award your <Bold text='effort' /> and track your < Bold text='spending' />
+            </Text>
             <Button variant="solid" className="mt-5" size='lg' rounded="pill" text='Get The App' icon={<AiFillAndroid />} />
         </div>
     )
