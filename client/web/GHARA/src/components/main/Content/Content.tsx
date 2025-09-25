6fb2690f7e36681c60994509a5e155c2bc4e0aed
yashweblife <yashweblife20@gmail.com>
2025-09-25 15:49:26 -0500

    new content component

---
+++ client/web/GHARA/src/components/main/Content/Content.tsx
@@ -0,0 +1,10 @@
+export default function Content() {
+    return (
+        <div className='bg-gray-900 h-200 px-20'>
+            <div className="rounded-2xl shadow-2xl ring-sky-950 bg-blue-950 w-full p-20">
+
+            </div>
+
+        </div>
+    )
+}
