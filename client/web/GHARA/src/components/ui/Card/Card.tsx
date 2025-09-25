785fc079814b58b86ea89cc055e60a42fad1c4a2
yashweblife <yashweblife20@gmail.com>
2025-09-25 15:55:46 -0500

    card

--- client/web/GHARA/src/components/ui/Card/Card.tsx
+++ client/web/GHARA/src/components/ui/Card/Card.tsx
@@ -1,5 +1,15 @@
-export default function Card() {
+import type { ReactNode } from "react"
+
+export function CardHeader() { }
+
+
+export type CardProps = {
+    children?: ReactNode
+}
+export default function Card({ children }: CardProps) {
     return (
-        <div></div>
+        <div className="rounded-2xl shadow-2xl ring-sky-950 bg-gray-900 w-full p-20">
+            {children}
+        </div>
     )
 }
