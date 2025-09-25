fc941d429e643cb21d6d59303ca5a3bfb9e04825
yashweblife <yashweblife20@gmail.com>
2025-09-25 16:45:38 -0500

    many changes

--- client/web/GHARA/src/components/ui/Card/Card.tsx
+++ client/web/GHARA/src/components/ui/Card/Card.tsx
@@ -1,14 +1,47 @@
-import type { ReactNode } from "react"
-
-export function CardHeader() { }
-
-
-export type CardProps = {
-    children?: ReactNode
+import type { BaseComponent } from "../../../lib"
+export type CardImageProps = {
+    src: string
+    className?: string
 }
-export default function Card({ children }: CardProps) {
+export function CardImage({ src, className = '' }: CardImageProps) {
+    const defaultClasses = "w-full"
+    const finalClasses = [defaultClasses, className].join(' ')
+    console.log('test', finalClasses)
     return (
-        <div className="rounded-2xl shadow-2xl ring-sky-950 bg-gray-900 w-full p-20">
+        <div className="w-full p-5">
+            <img src={src} alt="image" className={finalClasses} />
+        </div>
+    )
+}
+
+
+export type CardHeaderProps = BaseComponent & {
+
+}
+export function CardHeader({ children }: CardHeaderProps) {
+    return (
+        <div className="text-2xl p-5">
+            {children}
+        </div>
+    )
+}
+export type CardContentProps = BaseComponent & {
+}
+export function CardContent({ children }: CardContentProps) {
+    return (
+        <div className="p-5">
+            {children}
+        </div>
+    )
+}
+
+export type CardProps = BaseComponent & {
+}
+export default function Card({ children, className }: CardProps) {
+    const defaultClasses = "rounded-2xl shadow-2xl ring-sky-950 bg-gray-900 w-full"
+    const finalClasses = [defaultClasses, className].join(' ')
+    return (
+        <div className={finalClasses}>
             {children}
         </div>
     )
