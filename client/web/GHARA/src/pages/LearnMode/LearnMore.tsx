1660b10a96482914cfc53587598e0814c8685ee9
yashweblife <yashweblife20@gmail.com>
2025-09-26 12:45:43 -0500

    add content to header

--- client/web/GHARA/src/pages/LearnMode/LearnMore.tsx
+++ client/web/GHARA/src/pages/LearnMode/LearnMore.tsx
@@ -1,7 +1,15 @@
+import Header from "@/components/main/Header/Header";
+import Button from "@/components/ui/Button/Button";
+import { MdInstallMobile, MdOutlineAccountCircle, MdOutlineHome } from "react-icons/md";
+
 export default function Page() {
     return (
         <>
-
+            <Header title="Ghara | Learn More">
+                <Button icon={<MdOutlineHome />} size='sm' variant="ghost" text='Home' />
+                <Button icon={<MdInstallMobile />} size='sm' variant="outlined" text='Install' />
+                <Button icon={<MdOutlineAccountCircle />} size='sm' variant="solid" text='Login' />
+            </Header>
         </>
     )
 }
