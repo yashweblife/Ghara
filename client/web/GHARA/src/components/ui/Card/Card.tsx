ec67b833c4252f526758b256d91654ba4a9c542c
yashweblife <yashweblife20@gmail.com>
2025-09-26 12:50:44 -0500

    fix styles

--- client/web/GHARA/src/components/ui/Card/Card.tsx
+++ client/web/GHARA/src/components/ui/Card/Card.tsx
@@ -1,11 +1,11 @@
-import type { ReactNode } from "react"
 import type { BaseComponent } from "../../../lib"
-export type CardHolderProps = {
-    children?: ReactNode
+export type CardHolderProps = BaseComponent & {
 }
-export function CardHolder({ children }: CardHolderProps) {
+export function CardHolder({ children, className }: CardHolderProps) {
+    const defaultClasses = 'rounded-2xl shadow-2xl ring-sky-950 bg-cyan-950 w-full p-5 flex justify-center flex-col md:flex-row gap-5 flex-wrap'
+    const finalClasses = [defaultClasses, className].join(' ')
     return (
-        <div className="rounded-2xl shadow-2xl ring-sky-950 bg-cyan-950 w-full p-5 flex justify-center flex-col md:flex-row gap-5 flex-wrap">
+        <div className={finalClasses}>
             {children}
         </div>
     )
@@ -16,12 +16,12 @@ export type CardImageProps = {
     className?: string
 }
 export function CardImage({ src, className = '' }: CardImageProps) {
-    const defaultClasses = "w-full"
+    const defaultClasses = "w-full p-5 rounded-t-lg"
     const finalClasses = [defaultClasses, className].join(' ')
     console.log('test', finalClasses)
     return (
-        <div className="w-full p-5">
-            <img src={src} alt="image" className={finalClasses} />
+        <div className={finalClasses}>
+            <img src={src} alt="image" className='aspect-square max-h-100' />
         </div>
     )
 }
@@ -50,7 +50,7 @@ export function CardContent({ children }: CardContentProps) {
 export type CardProps = BaseComponent & {
 }
 export default function Card({ children, className }: CardProps) {
-    const defaultClasses = "rounded-2xl shadow-2xl ring-sky-950 bg-gray-900 w-full max-w-120"
+    const defaultClasses = "rounded-2xl shadow-2xl ring-sky-950 bg-gray-900 max-w-sm grid grid-cols-1"
     const finalClasses = [defaultClasses, className].join(' ')
     return (
         <div className={finalClasses}>
