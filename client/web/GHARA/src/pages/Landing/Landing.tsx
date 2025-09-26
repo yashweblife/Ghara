3ed43256e4b21a305d737c7f56ccc450aedf8e33
yashweblife <yashweblife20@gmail.com>
2025-09-26 12:29:00 -0500

    add landing page

---
+++ client/web/GHARA/src/pages/Landing/Landing.tsx
@@ -0,0 +1,13 @@
+import Content from "@/components/main/Content/Content";
+import Header from "@/components/main/Header/Header";
+import Hero from "@/components/main/Hero/Hero";
+
+export default function Page() {
+    return (
+        <>
+            <Header />
+            <Hero />
+            <Content />
+        </>
+    )
+}
