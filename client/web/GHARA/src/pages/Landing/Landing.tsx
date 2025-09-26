7daafb5cf7fae46d29c6ca54c9d209864d776ae2
yashweblife <yashweblife20@gmail.com>
2025-09-26 12:50:15 -0500

    more icons!!!!!

--- client/web/GHARA/src/pages/Landing/Landing.tsx
+++ client/web/GHARA/src/pages/Landing/Landing.tsx
@@ -2,7 +2,7 @@ import Content from "@/components/main/Content/Content";
 import Header from "@/components/main/Header/Header";
 import Hero from "@/components/main/Hero/Hero";
 import Button from "@/components/ui/Button/Button";
-import { MdInstallMobile, MdOutlineAccountCircle } from "react-icons/md";
+import { MdInstallMobile, MdOutlineAccountCircle, MdOutlineInfo } from "react-icons/md";
 import { Link } from "react-router-dom";
 
 export default function Page() {
@@ -10,7 +10,7 @@ export default function Page() {
         <>
             <Header>
                 <Link to='learn-more'>
-                    <Button variant="ghost" size='sm' text='Learn More' />
+                    <Button icon={<MdOutlineInfo />} variant="ghost" size='sm' text='Learn More' />
                 </Link>
                 <Button variant="outlined" size='sm' text='Install' icon={<MdInstallMobile />} />
                 <Button variant="solid" size='sm' text='Login' icon={<MdOutlineAccountCircle />} />
