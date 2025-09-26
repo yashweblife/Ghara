1de1fe7d72236f2ebf43197216a305486433abd8
yashweblife <yashweblife20@gmail.com>
2025-09-26 14:27:26 -0500

    fix path

--- client/web/GHARA/vite.config.ts
+++ client/web/GHARA/vite.config.ts
@@ -46,7 +46,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
-      '#': path.resolve(__dirname, 'public')
+      '#': path.resolve(__dirname, 'src/assets')
     }
   }
 });
