48ad24ee771adccd1f5d35ef65335c28c70e21ea
yashweblife <yashweblife20@gmail.com>
2025-09-26 12:30:45 -0500

    style fixes

--- client/web/GHARA/src/components/main/Content/Content.tsx
+++ client/web/GHARA/src/components/main/Content/Content.tsx
@@ -1,47 +1,53 @@
 import serverImage from '#/server-status.svg'
+import testImage from '#/test.svg'
 import todoImage from '#/to-do-app.svg'
 import visualImage from '#/visual-data.svg'
-
 import Card, { CardContent, CardHeader, CardHolder, CardImage } from "@/components/ui/Card/Card"
 import Text from "@/components/ui/Typography/Text/Text"
 export default function Content() {
     return (
-        <div className='bg-gray-900 min-h-200 p-5'>
-            <CardHolder>
-                <Card>
-                    <CardImage src={visualImage} />
-                    <CardHeader>
-                        <Text center>Visualize Your Weekly Spending</Text>
-                    </CardHeader>
-                    <CardContent>
-                        <Text center>
-                            Our app allows you to visualize you weekly spending and understand where you can save money
-                        </Text>
-                    </CardContent>
-                </Card>
-                <Card>
-                    <CardImage src={serverImage} />
-                    <CardHeader>
-                        <Text center>Self Host</Text>
-                    </CardHeader>
-                    <CardContent>
-                        <Text center>
-                            We want you to take control of your data instead of worrying about privacy, so setup the project locally
-                        </Text>
-                    </CardContent>
-                </Card>
-                <Card>
-                    <CardImage src={todoImage} className='h-115' />
-                    <CardHeader>
-                        <Text center>Make your Grocery Lists</Text>
-                    </CardHeader>
-                    <CardContent>
-                        <Text center>
-                            Our app enables you to better setup a grocery list and gives you an estimate of weekly cost based on what food you chose.
-                        </Text>
-                    </CardContent>
-                </Card>
-            </CardHolder>
+        <div className='bg-gray-900 min-h-200'>
+            <div className='p-5 md:py-0 md:pb-5 -translate-y-10 z-10 flex flex-col gap-5'>
+                <CardHolder className='backdrop-blur-2xl bg-sky-900/20'>
+                    <Text center className='text-4xl w-full py-5' bold>What We Offer</Text>
+                    <Card>
+                        <CardImage src={visualImage} />
+                        <CardHeader>
+                            <Text center>Visualize Your Weekly Spending</Text>
+                        </CardHeader>
+                        <CardContent>
+                            <Text center>
+                                Our app allows you to visualize you weekly spending and understand where you can save money
+                            </Text>
+                        </CardContent>
+                    </Card>
+                    <Card>
+                        <CardImage src={serverImage} />
+                        <CardHeader>
+                            <Text center>Self Host An Instance</Text>
+                        </CardHeader>
+                        <CardContent>
+                            <Text center>
+                                We want you to take control of your data instead of worrying about privacy, so setup the project locally
+                            </Text>
+                        </CardContent>
+                    </Card>
+                    <Card>
+                        <CardImage src={todoImage} />
+                        <CardHeader>
+                            <Text center>Make your Grocery Lists</Text>
+                        </CardHeader>
+                        <CardContent>
+                            <Text center>
+                                Our app enables you to better setup a grocery list and gives you an estimate of weekly cost based on what food you chose.
+                            </Text>
+                        </CardContent>
+                    </Card>
+                </CardHolder>
+            </div>
+            <div className='bg-gray-900'>
+                <img src={testImage} alt="background image" />
+            </div>
         </div>
     )
 }
