6a4d9717609de6e5b03553a005f9f24a237b7f5c
yashweblife <yashweblife20@gmail.com>
2025-09-30 16:05:13 +0000

    change tag

--- client/web/GHARA/src/components/main/Content/Content.tsx
+++ client/web/GHARA/src/components/main/Content/Content.tsx
@@ -7,8 +7,8 @@ export default function Content({ children, className }: ContentProps) {
     const defaultClasses = 'bg-gray-900 min-h-200'
     const finalClasses = [defaultClasses, className].join(' ')
     return (
-        <div className={finalClasses}>
+        <section className={finalClasses}>
             {children}
-        </div>
+        </section>
     )
 }
