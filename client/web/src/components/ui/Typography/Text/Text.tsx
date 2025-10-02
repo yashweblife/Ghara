0bb701ca39fd7132b6bcd724a00be66e7864da2c
yashweblife <yashweblife20@gmail.com>
2025-10-02 21:21:29 +0000

    add conditions

--- client/web/src/components/ui/Typography/Text/Text.tsx
+++ client/web/src/components/ui/Typography/Text/Text.tsx
@@ -4,12 +4,13 @@ export interface TextProps extends React.HTMLAttributes<HTMLParagraphElement> {
     children: ReactNode
     className?: string
     center?: boolean
+    responsive?: boolean
     bold?: boolean
 }
 
-export default function Text({ children, className = '', center = false, bold = false, ...props }: TextProps) {
+export default function Text({ children, className = '', center = false, responsive = true, bold = false, ...props }: TextProps) {
     const defaultClasses = 'dark:text-white'
-    const centered = center && 'text-center'
+    const centered = center ? responsive ? 'text-center md:text-left' : 'text-center' : ''
     const bolded = bold && 'font-bold'
     const finalClasses = [defaultClasses, className, centered, bolded].join(' ')
     return (
