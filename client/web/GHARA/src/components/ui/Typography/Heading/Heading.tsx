57b07d6685f0529ca0ab7079ba403d89c3f120e0
yashweblife <yashweblife20@gmail.com>
2025-10-02 22:23:22 +0000

    try new

--- client/web/GHARA/src/components/ui/Typography/Heading/Heading.tsx
+++ client/web/GHARA/src/components/ui/Typography/Heading/Heading.tsx
@@ -1,16 +1,25 @@
 import type { BaseComponent } from "@/lib";
+import { type JSX } from "react";
 
 export type HeadingProps = BaseComponent & {
-    variant?: string
+    variant?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
 }
-
-export default function Heading({ variant = 'h1', children, className = '' }: HeadingProps) {
-    const defaultClasses = ''
+const headingStyles: Record<NonNullable<HeadingProps["variant"]>, string> = {
+    h1: "text-4xl font-extrabold",
+    h2: "text-3xl font-bold",
+    h3: "text-2xl font-semibold",
+    h4: "text-xl font-semibold",
+    h5: "text-lg font-medium",
+    h6: "text-base font-medium uppercase tracking-wide",
+};
+export default function Heading({
+    variant = 'h1',
+    children,
+    className = '',
+    ...props
+}: HeadingProps) {
+    const Tag: keyof JSX.IntrinsicElements = variant
+    const defaultClasses = headingStyles[variant]
     const finalClasses = [defaultClasses, className].join(' ')
-    if (variant === 'h2') return <h2 className={finalClasses}>{children}</h2>
-    if (variant === 'h3') return <h3 className={finalClasses}>{children}</h3>
-    if (variant === 'h4') return <h4 className={finalClasses}>{children}</h4>
-    if (variant === 'h5') return <h5 className={finalClasses}>{children}</h5>
-    if (variant === 'h6') return <h6 className={finalClasses}>{children}</h6>
-    return <h1 className={finalClasses}>{children}</h1>
+    return (<Tag className={finalClasses}>{children}</Tag>)
 }
