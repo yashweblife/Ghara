0a69ba4e31511c44f84e663c40dda687d60532a4
yashweblife <yashweblife20@gmail.com>
2025-10-03 19:38:57 +0000

    fix

--- client/web/src/pages/Landing/Landing.tsx
+++ client/web/src/pages/Landing/Landing.tsx
@@ -24,10 +24,10 @@ export default function Page() {
                 <Button variant="solid" size='sm' text='Login' icon={<MdOutlineAccountCircle />} />
             </Header>
             <Hero>
-                <Text center className="text-3xl md:text-4xl text-shadow-xs drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]" bold>
+                <Text className="text-3xl md:text-4xl text-shadow-xs drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]" bold>
                     Take control of your <Bold text='Ghara' />
                 </Text>
-                <Text center className='md:w-[45ch] text-gray-500 md:text-2xl text-shadow-xs drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]'>
+                <Text className='md:w-[45ch] text-gray-500 md:text-2xl text-shadow-xs drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]'>
                     Manage your <Bold text='groceries' />, Award your <Bold text='effort' /> and track your < Bold text='spending' />
                 </Text>
                 <Button variant="solid" className="mt-5" size='lg' rounded="pill" text='Get The App' icon={<AiFillAndroid />} />
@@ -35,7 +35,7 @@ export default function Page() {
             <Content>
                 <div className='p-5 md:py-0 md:pb-5 -translate-y-10 z-10 flex flex-col gap-5'>
                     <CardHolder className='backdrop-blur-2xl bg-sky-900/20'>
-                        <Text center className='text-4xl w-full py-5' bold>What We Offer</Text>
+                        <Text className='text-4xl w-full py-5' bold>What We Offer</Text>
                         <Card>
                             <CardImage src={visualImage} />
                             <CardHeader>
