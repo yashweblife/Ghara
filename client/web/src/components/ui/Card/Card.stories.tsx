644b4787bc5c7f787bea632971587a43f9d97bc6
yashweblife <yashweblife20@gmail.com>
2025-10-04 21:38:32 +0000

    fix path for image

--- client/web/src/components/ui/Card/Card.stories.tsx
+++ client/web/src/components/ui/Card/Card.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
-import todoImage from '../../../../public/to-do-app.svg';
+import todoImage from '../../../assets/to-do-app.svg';
 
 import Text from '../Typography/Text/Text';
 import Card, { CardContent, CardHeader, CardImage } from './Card';
