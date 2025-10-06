96e4aea4c015046b89ebbcababeeccb6a2081ecc
yashweblife <yashweblife20@gmail.com>
2025-10-06 22:00:54 +0000

    add more stories

--- client/web/src/components/ui/Typography/Text/Text.stories.tsx
+++ client/web/src/components/ui/Typography/Text/Text.stories.tsx
@@ -13,6 +13,20 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   args: {
     children: 'Jello',
-    className: 'text-sky-950!'
+    className: 'text-sky-950!',
+  }
+};
+export const Center: Story = {
+  args: {
+    children: 'Jello',
+    className: 'text-sky-950!',
+    center: false,
+  }
+};
+export const Responsive: Story = {
+  args: {
+    children: 'Jello',
+    responsive: false,
+    className: 'text-sky-950!',
   }
 };
