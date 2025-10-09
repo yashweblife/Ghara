fc03bf822312dd104d19b141f77c9565624d438a
yashweblife <yashweblife20@gmail.com>
2025-10-09 18:18:12 +0000

    fix cnetering

--- client/web/src/pages/Landing/Landing.tsx
+++ client/web/src/pages/Landing/Landing.tsx
@@ -24,10 +24,10 @@ export default function Page() {
                 <Button variant="solid" size='sm' text='Login' icon={<MdOutlineAccountCircle />} />
             </Header>
             <Hero>
-                <Text className="text-3xl md:text-4xl text-shadow-xs drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]" bold>
+                <Text className="text-3xl md:text-4xl text-shadow-xs drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]" bold center>
                     Take control of your <Bold text='Ghara' />
                 </Text>
-                <Text className='md:w-[45ch] text-gray-500 md:text-2xl text-shadow-xs drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]'>
+                <Text className='md:w-[45ch] text-gray-500 md:text-2xl text-shadow-xs drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]' center responsive={false}>
                     Manage your <Bold text='groceries' />, Award your <Bold text='effort' /> and track your < Bold text='spending' />
                 </Text>
                 <Button variant="solid" className="mt-5" size='lg' rounded="pill" text='Get The App' icon={<AiFillAndroid />} />
@@ -35,14 +35,14 @@ export default function Page() {
             <Content>
                 <div className='p-5 md:py-0 md:pb-5 -translate-y-10 z-10 flex flex-col gap-5'>
                     <CardHolder className='backdrop-blur-2xl bg-sky-900/20'>
-                        <Text className='text-4xl w-full py-5' bold>What We Offer</Text>
+                        <Text className='text-4xl w-full py-5' center bold>What We Offer</Text>
                         <Card>
                             <CardImage src={visualImage} />
                             <CardHeader>
-                                <Text center>Visualize Your Weekly Spending</Text>
+                                <Text center responsive={false}>Visualize Your Weekly Spending</Text>
                             </CardHeader>
                             <CardContent>
-                                <Text center>
+                                <Text center responsive={false}>
                                     Our app allows you to visualize you weekly spending and understand where you can save money
                                 </Text>
                             </CardContent>
@@ -50,10 +50,10 @@ export default function Page() {
                         <Card>
                             <CardImage src={serverImage} />
                             <CardHeader>
-                                <Text center>Self Host An Instance</Text>
+                                <Text center responsive={false}>Self Host An Instance</Text>
                             </CardHeader>
                             <CardContent>
-                                <Text center>
+                                <Text center responsive={false}>
                                     We want you to take control of your data instead of worrying about privacy, so setup the project locally
                                 </Text>
                             </CardContent>
@@ -61,10 +61,10 @@ export default function Page() {
                         <Card>
                             <CardImage src={todoImage} />
                             <CardHeader>
-                                <Text center>Make your Grocery Lists</Text>
+                                <Text center responsive={false}>Make your Grocery Lists</Text>
                             </CardHeader>
                             <CardContent>
-                                <Text center>
+                                <Text center responsive={false}>
                                     Our app enables you to better setup a grocery list and gives you an estimate of weekly cost based on what food you chose.
                                 </Text>
                             </CardContent>
