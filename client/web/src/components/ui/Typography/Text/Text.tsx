21e35b079614e08250fe514ff46de086589bb393
yashweblife <yashweblife20@gmail.com>
2025-10-09 16:49:31 +0000

    fix

--- client/web/src/components/ui/Typography/Text/Text.tsx
+++ client/web/src/components/ui/Typography/Text/Text.tsx
@@ -1,20 +1,12 @@
 import type { ReactNode } from "react";
 
-export interface TextProps extends React.HTMLAttributes<HTMLParagraphElement> {
+export type TextProps = React.HTMLAttributes<HTMLParagraphElement> & {
     children: ReactNode
     className?: string
     center?: boolean
     responsive?: boolean
     bold?: boolean
 }
-/**
- * Text Component
- * - Center is false by default
- * - Responsive is true by default
- * - Bold is false by default
- * @param param0 
- * @returns 
- */
 export default function Text({ children, className = '', center = false, responsive = true, bold = false, ...props }: TextProps) {
     const defaultClasses = 'dark:text-white'
     const centered = center ? responsive ? 'text-center md:text-left' : 'text-center' : ''
