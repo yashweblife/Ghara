b86bb98847584a86b1a3d7f6d6ef88f5894d4b07
yashweblife <yashweblife20@gmail.com>
2025-10-09 22:30:06 +0000

    add autodocs

--- client/web/.storybook/preview.ts
+++ client/web/.storybook/preview.ts
@@ -1,6 +1,7 @@
 import type { Preview } from '@storybook/react-vite';
 import '../src/index.css';
 const preview: Preview = {
+    tags: ['autodocs'],
     parameters: {
         controls: {
             matchers: {
