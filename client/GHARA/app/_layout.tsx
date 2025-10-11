00745e68ef30e8f452151d961d3255b76be102d3
yashweblife <yashweblife20@gmail.com>
2025-10-10 20:28:49 -0500

    clean

--- client/GHARA/app/_layout.tsx
+++ client/GHARA/app/_layout.tsx
@@ -3,10 +3,6 @@ import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
-
 export default function RootLayout() {
 
   return (
