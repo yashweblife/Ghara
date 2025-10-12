23c9f365ddddc07df6e8a30a1e20716837b731b4
yashweblife <yashweblife20@gmail.com>
2025-10-12 14:58:19 -0500

    test

--- client/GHARA/app/(tabs)/index.tsx
+++ client/GHARA/app/(tabs)/index.tsx
@@ -1,5 +1,10 @@
+import { Text, View } from "react-native";
 export default function HomeScreen() {
   return (
-    <></>
+    <View>
+      <Text>
+        Hello World
+      </Text>
+    </View>
   );
 }
